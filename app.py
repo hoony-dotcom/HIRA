e0e38e1420ff35d2c598952698bb3648b05abfd3
hoony-dotcom <c2619350@gmail.com>
2026-09-21 15:47:26 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ else:
             with st.expander("📌 의용공학팀 개발앱 바로가기", expanded=False):
                 st.markdown("- [1. 의료장비 투자집행 계획 실적](https://buly.kr/DEbvdwF)")
                 st.markdown("- [2. 의료장비 현황 바로가기](https://buly.kr/7mERs3u)")
-                st.markdown("- [3. 건강보험심사평가원 의료장비 상세현황 조회](https://buly.kr/7mERs3u)")
+                st.markdown("- [3. 심평원 신고 장비 상세현황](https://buly.kr/uWvRbg)")
+                st.markdown("- [4. 건강보험심사평가원 의료장비 상세현황 조회](https://buly.kr/7mERs3u)")
 
             # 2. 제작 및 문의 정보
             with st.expander("🛠️ 제작 및 문의 정보", expanded=False):
@@ -132,7 +133,7 @@ else:
             
             # 정렬 기준 선택 (기본값을 '모델명'으로 설정 -> 인덱스 1번)
             sort_by = st.radio("정렬 기준 선택", ["요양기관명", "모델명"], index=1)
-            sort_order = st.radio("정렬 방향", ["오름차순", "내름차순"])
+            sort_order = st.radio("정렬 방향", ["오름차순", "내림차순"])
             is_ascending = (sort_order == "오름차순")
 
         # 정렬 오류 방지를 위해 정렬 대상 컬럼을 문자열(str)로 변환 후 정렬 처리
